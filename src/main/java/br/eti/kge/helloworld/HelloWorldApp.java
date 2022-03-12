e19c2a66062b608056511d41cd6aefa24d817a55
kgeeti <kge@terra.com.br>
2022-03-12 17:56:09 -0300

    Inclusão do método main e mensagens de boas vindas. v1.0

---
+++ src/main/java/br/eti/kge/helloworld/HelloWorldApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.eti.kge.helloworld;
+
+/**
+ *
+ * @author KGe
+ */
+public class HelloWorldApp {
+     public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Parabens! Você importou com sucesso o projeto HelloWorld!!!");
+    }
+}
